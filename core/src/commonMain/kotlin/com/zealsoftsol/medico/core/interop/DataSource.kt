33f9253c67e509e5b252cd76c5415d04fdca2343
Arnis <arnis_shaykh@ams-apps.com>
2020-11-30 14:48:09 +0500

    fix datasource for ios

--- core/src/commonMain/kotlin/com/zealsoftsol/medico/core/interop/DataSource.kt
+++ core/src/commonMain/kotlin/com/zealsoftsol/medico/core/interop/DataSource.kt
@@ -1,10 +1,14 @@
 package com.zealsoftsol.medico.core.interop
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-open class DataSource<T>(initialValue: T) {
-    protected val stateFlow = MutableStateFlow(initialValue)
+class DataSource<T>(initialValue: T) {
+    private val stateFlow = MutableStateFlow(initialValue)
     internal var value: T
         get() = stateFlow.value
         internal set(value) {
@@ -12,4 +16,15 @@ open class DataSource<T>(initialValue: T) {
         }
     val flow: StateFlow<T>
         get() = stateFlow
+
+    /**
+     * for iOS only
+     */
+    fun observeOnUi(onChange: (T) -> Unit) = observe(MainScope(), onChange)
+
+    private fun observe(scope: CoroutineScope, onChange: (T) -> Unit) {
+        stateFlow.onEach {
+            onChange(it)
+        }.launchIn(scope)
+    }
 }
