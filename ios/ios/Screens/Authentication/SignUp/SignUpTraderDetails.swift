9d824bcfc0bb3f136440847b9685cf3d6c97e1c1
Dasha Gurinovich <dasha.gurinovich@zpoint.app>
2020-12-07 13:57:42 +0300

    Updated the trader details screen input fields for different types of users

--- ios/ios/Screens/Authentication/SignUp/SignUpTraderDetails.swift
+++ ios/ios/Screens/Authentication/SignUp/SignUpTraderDetails.swift
@@ -28,51 +28,9 @@ struct SignUpTraderDetails: View {
     
     var traderDataFields: some View {
         VStack(spacing: 12) {
-            let tradeNameErrorMessageKey = self.validation.value?.tradeName
-            FloatingPlaceholderTextField(placeholderLocalizedStringKey: "trade_name",
-                                         text: self.registration.value?.tradeName,
-                                         onTextChange: { newValue in scope.changeTradeName(tradeName: newValue) },
-                                         isValid: tradeNameErrorMessageKey == nil,
-                                         errorMessageKey: tradeNameErrorMessageKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.words)
-            
-            let panNumberErrorMessageKey = self.validation.value?.panNumber
-            FloatingPlaceholderTextField(placeholderLocalizedStringKey: "pan_number",
-                                         text: self.registration.value?.panNumber,
-                                         onTextChange: { newValue in scope.changePan(panNumber: newValue) },
-                                         isValid: panNumberErrorMessageKey == nil,
-                                         errorMessageKey: panNumberErrorMessageKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-
-            let gstinErrorMessageKey = self.validation.value?.gstin
-            FloatingPlaceholderTextField(placeholderLocalizedStringKey: "gstin",
-                                         text: self.registration.value?.gstin,
-                                         onTextChange: { newValue in scope.changeGstin(gstin: newValue) },
-                                         isValid: gstinErrorMessageKey == nil,
-                                         errorMessageKey: gstinErrorMessageKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-            
-            let drugLicenseNo1ErrorMessageKey = self.validation.value?.drugLicenseNo1
-            FloatingPlaceholderTextField(placeholderLocalizedStringKey: "drug_license_No1",
-                                         text: self.registration.value?.drugLicenseNo1,
-                                         onTextChange: { newValue in scope.changeDrugLicense1(drugLicenseNo: newValue) },
-                                         isValid: drugLicenseNo1ErrorMessageKey == nil,
-                                         errorMessageKey: drugLicenseNo1ErrorMessageKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-            
-            let drugLicenseNo2ErrorMessageKey = self.validation.value?.drugLicenseNo2
-            FloatingPlaceholderTextField(placeholderLocalizedStringKey: "drug_license_No2",
-                                         text: self.registration.value?.drugLicenseNo2,
-                                         onTextChange: { newValue in scope.changeDrugLicense2(drugLicenseNo: newValue) },
-                                         isValid: drugLicenseNo2ErrorMessageKey == nil,
-                                         errorMessageKey: drugLicenseNo2ErrorMessageKey)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-            
+            ForEach(scope.inputFields, id: \.self) {
+                getInputField(forType: $0)
+            }
         }
         .modifier(ScrollViewModifier())
     }
@@ -82,6 +40,69 @@ struct SignUpTraderDetails: View {
         
         self.registration = SwiftDatasource(dataSource: scope.registration)
         self.validation = SwiftDatasource(dataSource: scope.validation)
+    }
+    
+    private func getInputField(forType type: SignUpScope.TraderDataFields) -> some View {
+        switch type {
+        
+        case SignUpScope.TraderDataFields.tradeName:
+            let tradeNameErrorMessageKey = self.validation.value?.tradeName
+            return AnyView(
+                FloatingPlaceholderTextField(placeholderLocalizedStringKey: "trade_name",
+                                             text: self.registration.value?.tradeName,
+                                             onTextChange: { newValue in scope.changeTradeName(tradeName: newValue) },
+                                             isValid: tradeNameErrorMessageKey == nil,
+                                             errorMessageKey: tradeNameErrorMessageKey)
+                .disableAutocorrection(true)
+                .autocapitalization(.words))
+            
+        case SignUpScope.TraderDataFields.pan:
+            let panNumberErrorMessageKey = self.validation.value?.panNumber
+            return AnyView(
+                FloatingPlaceholderTextField(placeholderLocalizedStringKey: "pan_number",
+                                             text: self.registration.value?.panNumber,
+                                             onTextChange: { newValue in scope.changePan(panNumber: newValue) },
+                                             isValid: panNumberErrorMessageKey == nil,
+                                             errorMessageKey: panNumberErrorMessageKey)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none))
+            
+        case SignUpScope.TraderDataFields.gstin:
+            let gstinErrorMessageKey = self.validation.value?.gstin
+            return AnyView(
+                FloatingPlaceholderTextField(placeholderLocalizedStringKey: "gstin",
+                                             text: self.registration.value?.gstin,
+                                             onTextChange: { newValue in scope.changeGstin(gstin: newValue) },
+                                             isValid: gstinErrorMessageKey == nil,
+                                             errorMessageKey: gstinErrorMessageKey)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none))
+            
+        case SignUpScope.TraderDataFields.license1:
+            let drugLicenseNo1ErrorMessageKey = self.validation.value?.drugLicenseNo1
+            return AnyView(
+                FloatingPlaceholderTextField(placeholderLocalizedStringKey: "drug_license_No1",
+                                             text: self.registration.value?.drugLicenseNo1,
+                                             onTextChange: { newValue in scope.changeDrugLicense1(drugLicenseNo: newValue) },
+                                             isValid: drugLicenseNo1ErrorMessageKey == nil,
+                                             errorMessageKey: drugLicenseNo1ErrorMessageKey)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none))
+            
+        case SignUpScope.TraderDataFields.license2:
+            let drugLicenseNo2ErrorMessageKey = self.validation.value?.drugLicenseNo2
+            return AnyView(
+                FloatingPlaceholderTextField(placeholderLocalizedStringKey: "drug_license_No2",
+                                             text: self.registration.value?.drugLicenseNo2,
+                                             onTextChange: { newValue in scope.changeDrugLicense2(drugLicenseNo: newValue) },
+                                             isValid: drugLicenseNo2ErrorMessageKey == nil,
+                                             errorMessageKey: drugLicenseNo2ErrorMessageKey)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none))
+            
+        default:
+            return AnyView(EmptyView())
+        }
     }
     
     private func tryToSignUp() {
