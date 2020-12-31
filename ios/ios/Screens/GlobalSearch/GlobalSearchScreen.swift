3592f332e11b0d95cf09605bad372301d249158e
Dasha Gurinovich <darya.gurinovich@interaxy.com>
2020-12-31 16:12:39 +0300

    Started the filter view implementation

--- ios/ios/Screens/GlobalSearch/GlobalSearchScreen.swift
+++ ios/ios/Screens/GlobalSearch/GlobalSearchScreen.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import core
+import GridStack
 
 extension SearchScope {
     override var navigationBarTintColor: UIColor? { return nil }
@@ -16,10 +17,38 @@ extension SearchScope {
 struct GlobalSearchScreen: View {
     let scope: SearchScope
     
+    @ObservedObject var isFilterOpened: SwiftDataSource<KotlinBoolean>
+    
+    @ObservedObject var filters: SwiftDataSource<NSArray>
+    
     var body: some View {
-        Text("Hello!")
-        .keyboardResponder()
-        .navigationBar(withNavigationBarContent: AnyView(searchBarPanel))
+        guard let isFilterOpened = self.isFilterOpened.value else {
+            return AnyView(EmptyView())
+        }
+        
+        let view: AnyView
+        
+        if isFilterOpened == true {
+            view = AnyView(self.filtersView)
+        }
+        else {
+            view = AnyView(self.productsView)
+        }
+        
+        return AnyView(
+            view
+                .scrollView()
+                .padding([.horizontal, .top], 16)
+                .keyboardResponder()
+                .navigationBar(withNavigationBarContent: AnyView(searchBarPanel))
+        )
+    }
+    
+    init(scope: SearchScope) {
+        self.scope = scope
+        
+        self.isFilterOpened = SwiftDataSource(dataSource: scope.isFilterOpened)
+        self.filters = SwiftDataSource(dataSource: scope.filters)
     }
     
     private var searchBarPanel: some View {
@@ -33,5 +62,68 @@ struct GlobalSearchScreen: View {
             .medicoText(fontSize: 17,
                         color: .blue)
         }
+    }
+    
+    private var filtersView: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                LocalizedText(localizedStringKey: "clear_all",
+                              textWeight: .medium,
+                              fontSize: 16,
+                              color: .textGrey,
+                              multilineTextAlignment: .trailing)
+                    .onTapGesture {
+                        scope.clearFilter(filter: nil)
+                    }
+            }
+            
+            if let filters = self.filters.value as? [DataFilter] {
+                ForEach(filters, id: \.self.name) { filter in
+                    getFilterView(for: filter)
+                }
+            }
+        }
+    }
+    
+    private func getFilterView(for filter: DataFilter) -> AnyView {
+        AnyView(
+            VStack {
+                AppColor.lightGrey.color
+                    .frame(height: 1)
+                
+                HStack {
+                    LocalizedText(localizedStringKey: filter.name,
+                                  textWeight: .semiBold,
+                                  fontSize: 16)
+                    
+                    Spacer()
+                    
+                    LocalizedText(localizedStringKey: "clear",
+                                  textWeight: .medium,
+                                  fontSize: 16,
+                                  color: .textGrey)
+                        .onTapGesture {
+                            scope.clearFilter(filter: filter)
+                        }
+                }
+                
+                // Remove the scroll bar
+                GridStack(minCellWidth: 100,
+                          spacing: 20,
+                          numItems: filter.options.count) { index, cellWidth in
+                    ZStack {
+                        AppColor.yellow.color
+                        LocalizedText(localizedStringKey: filter.options[index].value! as String)
+                    }
+                        .frame(width: cellWidth, height: 30)
+                }
+            }
+        )
+    }
+    
+    private var productsView: some View {
+        Text("Products")
     }
 }
