29471a5df3e0b3c27a0ecc0009c5858b2dccb794
Dasha Gurinovich <darya.gurinovich@interaxy.com>
2021-06-23 13:47:42 +0300

    MEDMD-421 Changed all 'Reject' to 'Decline'

--- ios/ios/AppDelegate.swift
+++ ios/ios/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     private func setUpAppNavigator() {
         #if DEBUG
-        let useMocks = true//false
+        let useMocks = false
         let useNavigatorSafeCasts = false
         let useNetworkInterceptor = true
         #else
