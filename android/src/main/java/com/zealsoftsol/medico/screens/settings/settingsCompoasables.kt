1894a3f3f4fad492adb46520152ec7328d66bdcb
anirudh <anirudh_sharma@softprodigy.com>
2021-12-15 19:42:17 +0530

    added views on the whatsapp screen, submit button pending

--- android/src/main/java/com/zealsoftsol/medico/screens/settings/settingsCompoasables.kt
+++ android/src/main/java/com/zealsoftsol/medico/screens/settings/settingsCompoasables.kt
@@ -1,20 +1,34 @@
 package com.zealsoftsol.medico.screens.settings
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zealsoftsol.medico.ConstColors
 import com.zealsoftsol.medico.R
 import com.zealsoftsol.medico.core.mvi.scope.nested.SettingsScope
 import com.zealsoftsol.medico.data.AddressData
 import com.zealsoftsol.medico.data.User
 import com.zealsoftsol.medico.screens.common.*
 
-@Preview
 @Composable
 fun SettingsScreen(scope: SettingsScope) {
     Column(
@@ -32,7 +46,6 @@ fun SettingsScreen(scope: SettingsScope) {
     }
 }
 
-@Preview
 @Composable
 private fun SettingsList(sections: List<SettingsScope.List.Section>) {
     sections.forEach {
@@ -54,7 +67,6 @@ private fun SettingsList(sections: List<SettingsScope.List.Section>) {
     }
 }
 
-@Preview
 @Composable
 private fun Profile(user: User) {
     ReadOnlyField(user.firstName, R.string.first_name)
@@ -66,7 +78,6 @@ private fun Profile(user: User) {
     ReadOnlyField(user.phoneNumber.formatIndia(), R.string.phone_number)
 }
 
-@Preview
 @Composable
 private fun Address(addressData: AddressData) {
     ReadOnlyField(addressData.pincode.toString(), R.string.pincode)
@@ -84,7 +95,6 @@ private fun Address(addressData: AddressData) {
     ReadOnlyField(addressData.state, R.string.state)
 }
 
-@Preview
 @Composable
 private fun GstinDetails(details: User.Details.DrugLicense) {
     ReadOnlyField(details.tradeName, R.string.trade_name)
@@ -96,14 +106,77 @@ private fun GstinDetails(details: User.Details.DrugLicense) {
     ReadOnlyField(details.license2, R.string.drug_license_2)
 }
 
-@Preview
+
 @Composable
 private fun WhatsappPreference() {
-    ReadOnlyField("name", R.string.trade_name)
-    Space(12.dp)
-    ReadOnlyField("tin", R.string.gstin)
-    Space(12.dp)
-    ReadOnlyField("details.license1", R.string.drug_license_1)
-    Space(12.dp)
-    ReadOnlyField("details.license2", R.string.drug_license_2)
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Space(12.dp)
+        Text(
+            text = stringResource(id = R.string.preferred_language),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.W500,
+            color = Color.Black,
+        )
+        Space(12.dp)
+        LanguagePicker(
+            hint = stringResource(id = R.string.language_english),
+        )
+        Space(20.dp)
+        Text(
+            text = stringResource(id = R.string.phone_number),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.W500,
+            color = Color.Black,
+        )
+        Space(12.dp)
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White, shape = MaterialTheme.shapes.medium),
+            value = text,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                textColor = Color.Black,
+                placeholderColor = Color.Black,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { text = it },
+            placeholder = { Text("+91") }
+        )
+    }
+}
+
+@Composable
+private fun LanguagePicker(hint: String) {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        val context = LocalContext.current
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White, shape = MaterialTheme.shapes.medium)
+                .clickable(onClick = {
+                })
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                text = null ?: hint,
+                color = Color.Black,
+                fontSize = 14.sp,
+            )
+            Icon(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = null,
+                tint = ConstColors.gray,
+            )
+        }
+    }
+
 }
