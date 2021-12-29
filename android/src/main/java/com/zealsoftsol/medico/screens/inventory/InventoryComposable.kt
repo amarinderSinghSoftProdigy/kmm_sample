ed79b9c499de9746ea71bdb7e13bdaed18ec0d13
anirudh <anirudh_sharma@softprodigy.com>
2021-12-29 15:33:31 +0530

    adjusted constraints for availability and expiry view

--- android/src/main/java/com/zealsoftsol/medico/screens/inventory/InventoryComposable.kt
+++ android/src/main/java/com/zealsoftsol/medico/screens/inventory/InventoryComposable.kt
@@ -2,6 +2,7 @@ package com.zealsoftsol.medico.screens.inventory
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -42,19 +43,29 @@ fun InventoryMainComposable(scope: InventoryScope) {
                 .padding(16.dp)
         ) {
             item {
-                StatusView(scope = scope, modifier = Modifier
-                    .fillParentMaxWidth()
-                    .padding(8.dp), 100f)
+                StatusView(
+                    scope = scope, modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(8.dp), 100f
+                )
             }
             item {
-                AvailabilityAndExpiryView(scope = scope, modifier = Modifier
-                    .fillParentMaxWidth()
-                    .padding(8.dp) ,70f)
+                AvailabilityView(
+                    scope = scope,
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(8.dp),
+                    70f,
+                )
             }
             item {
-                AvailabilityAndExpiryView(scope = scope, modifier = Modifier
-                    .fillParentMaxWidth()
-                    .padding(8.dp), 70f)
+                ExpiryView(
+                    scope = scope,
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(8.dp),
+                    70f,
+                )
             }
         }
     }
@@ -114,8 +125,8 @@ fun StatusView(scope: InventoryScope, modifier: Modifier, sliceThickness: Float)
                     )
                 ),
                 modifier = Modifier.constrainAs(chart) {
-                    width = Dimension.value(150.dp)
-                    height = Dimension.value(150.dp)
+                    width = Dimension.value(140.dp)
+                    height = Dimension.value(140.dp)
                     end.linkTo(parent.end, margin = 5.dp)
                     bottom.linkTo(parent.bottom, margin = 10.dp)
                     top.linkTo(parent.top, margin = 10.dp)
@@ -127,10 +138,14 @@ fun StatusView(scope: InventoryScope, modifier: Modifier, sliceThickness: Float)
 
 
 /**
- * Status of online and offline
+ * Availability view
  */
 @Composable
-fun AvailabilityAndExpiryView(scope: InventoryScope, modifier: Modifier, sliceThickness: Float) {
+fun AvailabilityView(
+    scope: InventoryScope,
+    modifier: Modifier,
+    sliceThickness: Float,
+) {
     Card(
         modifier = modifier,
         elevation = 5.dp,
@@ -142,9 +157,9 @@ fun AvailabilityAndExpiryView(scope: InventoryScope, modifier: Modifier, sliceTh
                 .fillMaxWidth()
                 .padding(start = 10.dp)
         ) {
-            val (status, online, offline, chart) = createRefs()
+            val (status, first, second, third, chart) = createRefs()
             Text(
-                text = stringResource(R.string.status),
+                text = stringResource(id = R.string.availability),
                 color = Color.Black,
                 fontSize = 15.sp,
                 fontWeight = FontWeight.W700,
@@ -153,18 +168,27 @@ fun AvailabilityAndExpiryView(scope: InventoryScope, modifier: Modifier, sliceTh
                     top.linkTo(parent.top, margin = 5.dp)
                 }
             )
-            CommonRoundedView(
-                text = "${stringResource(R.string.online)}: 1000",
-                modifier = Modifier.constrainAs(online) {
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.in_stock)}: 1000",
+                modifier = Modifier.constrainAs(first) {
                     start.linkTo(parent.start, margin = 5.dp)
-                    bottom.linkTo(offline.top, margin = 5.dp)
+                    bottom.linkTo(second.top, margin = 5.dp)
                 },
                 color = ConstColors.darkGreen
             )
 
-            CommonRoundedView(
-                text = "${stringResource(R.string.offline)}: 1000",
-                modifier = Modifier.constrainAs(offline) {
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.limited_stock)}: 1000",
+                modifier = Modifier.constrainAs(second) {
+                    start.linkTo(parent.start, margin = 5.dp)
+                    bottom.linkTo(third.top, margin = 5.dp)
+                },
+                color = ConstColors.orange
+            )
+
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.out_stock)}: 1000",
+                modifier = Modifier.constrainAs(third) {
                     start.linkTo(parent.start, margin = 5.dp)
                     bottom.linkTo(parent.bottom, margin = 10.dp)
                 },
@@ -175,12 +199,14 @@ fun AvailabilityAndExpiryView(scope: InventoryScope, modifier: Modifier, sliceTh
                 thickness = sliceThickness,
                 listPieChartData = listOf(
                     PieChartData.Slice(10f, ConstColors.darkRed), PieChartData.Slice(
-                        70f, ConstColors.darkGreen), PieChartData.Slice(
-                        20f, ConstColors.orange)
+                        70f, ConstColors.darkGreen
+                    ), PieChartData.Slice(
+                        20f, ConstColors.orange
+                    )
                 ),
                 modifier = Modifier.constrainAs(chart) {
-                    width = Dimension.value(150.dp)
-                    height = Dimension.value(150.dp)
+                    width = Dimension.value(140.dp)
+                    height = Dimension.value(140.dp)
                     end.linkTo(parent.end, margin = 5.dp)
                     bottom.linkTo(parent.bottom, margin = 10.dp)
                     top.linkTo(parent.top, margin = 10.dp)
@@ -191,17 +217,102 @@ fun AvailabilityAndExpiryView(scope: InventoryScope, modifier: Modifier, sliceTh
 }
 
 /**
+ * Expiry view
+ */
+
+@Composable
+fun ExpiryView(
+    scope: InventoryScope,
+    modifier: Modifier,
+    sliceThickness: Float,
+) {
+    Card(
+        modifier = modifier,
+        elevation = 5.dp,
+        shape = RoundedCornerShape(5.dp),
+        backgroundColor = Color.White,
+    ) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp)
+        ) {
+            val (status, first, second, third, chart) = createRefs()
+            Text(
+                text = stringResource(id = R.string.expiry),
+                color = Color.Black,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.W700,
+                modifier = Modifier.constrainAs(status) {
+                    start.linkTo(parent.start, margin = 5.dp)
+                    top.linkTo(parent.top, margin = 5.dp)
+                }
+            )
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.long_expiry)}: 1000",
+                modifier = Modifier.constrainAs(first) {
+                    start.linkTo(parent.start, margin = 5.dp)
+                    bottom.linkTo(second.top, margin = 5.dp)
+                },
+                color = ConstColors.lightBlue
+            )
+
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.near_expiry)}: 1000",
+                modifier = Modifier.constrainAs(second) {
+                    start.linkTo(parent.start, margin = 5.dp)
+                    bottom.linkTo(third.top, margin = 5.dp)
+                },
+                color = ConstColors.orange
+            )
+
+            ColorIndicatorTextView(
+                text = "${stringResource(R.string.expired)}: 1000",
+                modifier = Modifier.constrainAs(third) {
+                    start.linkTo(parent.start, margin = 5.dp)
+                    bottom.linkTo(parent.bottom, margin = 10.dp)
+                },
+                color = ConstColors.darkRed
+            )
+
+            MyChartParent(
+                thickness = sliceThickness,
+                listPieChartData = listOf(
+                    PieChartData.Slice(10f, ConstColors.lightBlue), PieChartData.Slice(
+                        70f, ConstColors.darkGreen
+                    ), PieChartData.Slice(
+                        20f, ConstColors.orange
+                    )
+                ),
+                modifier = Modifier.constrainAs(chart) {
+                    width = Dimension.value(140.dp)
+                    height = Dimension.value(140.dp)
+                    end.linkTo(parent.end, margin = 5.dp)
+                    bottom.linkTo(parent.bottom, margin = 10.dp)
+                    top.linkTo(parent.top, margin = 10.dp)
+                }
+            )
+        }
+    }
+}
+
+
+/**
  * draw chartview
  * @param modifier Modifiers to be applied to the chart view
  * @param thickness thickness of pie chart arcs (0-100f)
  * @param listPieChartData list of {@PieChartData.Slice} containing the values to be drawn
  */
 @Composable
-fun MyChartParent(modifier: Modifier, thickness: Float, listPieChartData: List<PieChartData.Slice>) {
+fun MyChartParent(
+    modifier: Modifier,
+    thickness: Float,
+    listPieChartData: List<PieChartData.Slice>
+) {
 
     PieChart(
         pieChartData = PieChartData(
-           listPieChartData
+            listPieChartData
         ),
         modifier = modifier,
         animation = simpleChartAnimation(),
@@ -239,6 +350,18 @@ fun CommonRoundedView(
  */
 
 @Composable
-fun ColorIndicatorTextView(){
+fun ColorIndicatorTextView(color: Color, text: String, modifier: Modifier) {
+
+    Row(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(3.dp))
+                .background(color)
+                .padding(3.dp)
+        )
+
+        Text(text = text, color = Color.Black, fontSize = 14.sp)
+
+    }
 
 }
