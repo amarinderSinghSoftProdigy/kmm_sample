4a320ac3df656d8895a03d77020bf54d5da730a9
anirudh <anirudh_sharma@softprodigy.com>
2022-03-08 16:04:21 +0530

    password validation added

--- android/src/main/java/com/zealsoftsol/medico/screens/password/passwordComposables.kt
+++ android/src/main/java/com/zealsoftsol/medico/screens/password/passwordComposables.kt
@@ -39,14 +39,18 @@ fun EnterNewPasswordScreen(scope: PasswordScope.EnterNew) {
     val password1 = scope.password.flow.collectAsState()
     val password2 = scope.confirmPassword.flow.collectAsState()
     val validation = scope.passwordValidation.flow.collectAsState()
+    val password = scope.isValidPassword(password1.value)
+
     BasicScreen(
         subtitle = stringResource(id = R.string.new_password_hint),
         body = {
-            PasswordFormatInputField(
-                hint = stringResource(id = R.string.new_password),
-                text = password1.value,
-                onValueChange = { scope.changePassword(it) },
-            )
+            InputWithError(errorText = if (!password) stringResource(id = R.string.password_requirement) else null) {
+                PasswordFormatInputField(
+                    hint = stringResource(id = R.string.new_password),
+                    text = password1.value,
+                    onValueChange = { scope.changePassword(it) },
+                )
+            }
             Spacer(modifier = Modifier.size(12.dp))
             val isValid = password2.value.isEmpty() || password1.value == password2.value
             val errorText = validation.value?.password ?: run {
@@ -60,7 +64,7 @@ fun EnterNewPasswordScreen(scope: PasswordScope.EnterNew) {
                     onValueChange = { scope.changeConfirmPassword(it) },
                 )
             }
-            password1.value.isNotEmpty() && password1.value == password2.value
+            password && password1.value == password2.value
         },
         buttonText = stringResource(id = R.string.confirm),
         onButtonClick = { scope.submit() }
